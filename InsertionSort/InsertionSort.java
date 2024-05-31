f1aacd5ee208692673947b871aa04c37515e1b7f
Shanaya Hassen <gshanayahassen@gmail.com>
2024-05-31 06:17:18 +0530

    InsertionSort
    Insertion Sort Java Code

---
+++ InsertionSort/InsertionSort.java
@@ -0,0 +1,19 @@
+public class InsertionSort{
+    public static void main(String[] args) {
+        int arr[] = {15,6,10,3,5};
+
+        for(int i=0; i<arr.length; i++){
+            int curVal = arr[i];
+            int j = i=1;
+
+            while(j>=0 && arr[j]>curVal){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = curVal;
+        }
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
