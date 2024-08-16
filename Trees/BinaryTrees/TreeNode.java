995aaed9d266ea9eb27f7808871ece112467aa6d
Shanaya Hassen <gshanayahassen@gmail.com>
2024-08-16 19:09:45 +0530

    BinaryTree
    Binary Tree Implementation - Java

---
+++ Trees/BinaryTrees/TreeNode.java
@@ -0,0 +1,11 @@
+public class TreeNode {
+    char data;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(char data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
